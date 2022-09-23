c11360aecc0e18ef886d2adea4b26a57ffa832b6
xxjcaxx <jocasal@gmail.com>
2022-09-23 15:36:48 +0200

    exercicis objectes

---
+++ 01-Javascript/objectes/script.js
@@ -0,0 +1,50 @@
+/*
+
+Exercici 1: 
+* Crea un objecte que continga informació d'una pelicula: títol, duració i actors (un array de strings)
+* Crea una funció dins de l'objecte que imprimisca per la consola una frase amb aquests atributs.
+
+*/
+
+
+/*
+
+Exercici 2:
+* Crea una funció que accepta un objecte que representa un carret de la compra i treu per la consola el total de la compra:
+// Input
+let carret = {  
+  platans: "1.25",
+  mocadors: ".99",
+  samarreta: "25.01",
+  poma: "0.60",
+  gots: "10.34",
+  carn: "22.36"
+};
+
+// Output
+cashRegister(carret)); // 60.55
+
+*/
+
+/*
+Exercici 3:
+* Fes una classe que represente a un cilindre. El seu constructor acceptarà el diametre i l'alçada del cilindre. 
+* La classe tindrà una funció que retornarà el volum del cilindre (V = πr2h)
+*/
+
+/*
+Exercici 4: 
+* Fes una funció que imprimisca per la consola totes les funcions d'un objecte (no altres atributs)
+*/
+
+/*
+Exercici 5:
+* Fes una funció que accepte dos objectes sense atributs que siguen objectes o arrays i diga si són exàctament iguals
+*/
+
+/*
+Exercici 6: 
+* Fes una funció que indique si un objecte està buit profundament.
+* Per exemple: {a: null, b: '', c: undefined, d: [], e: {}, f: {a: null}} està buit. Observa que l'atribut f conté un objecte buit també. 
+*/
+
