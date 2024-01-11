63881367b093cfd6c0fa5248408ffae910f4f1fc
xxjcaxx <jocasal@gmail.com>
2024-01-11 16:53:31 +0100

    exempleholamon

--- 08 - angular/exemples2023/hola-mon/src/app/app.component.ts
+++ 08 - angular/exemples2023/hola-mon/src/app/app.component.ts
@@ -1,14 +1,26 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { MainComponent } from './components/main/main.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule, 
+    RouterOutlet, 
+    HeaderComponent, 
+    FooterComponent, 
+    MainComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'hola-mon';
+ nombre = 'Pepe';
+ apellido = 'Lopez';
+
 }
