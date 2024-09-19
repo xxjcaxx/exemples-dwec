6c1dc127d54005a3ba3b9cac5c111c80ceb1a6cb
xxjcaxx <jocasal@gmail.com>
2024-09-19 20:35:37 +0200

    exercici pokemons

--- 01-Javascript/iteradors/scripts.js
+++ 01-Javascript/iteradors/scripts.js
@@ -1,22 +1,31 @@
 import { pokemons } from "./iteradors.js";
 
+function calcPower(pokemon){
+  const poders = Object.values(pokemon.base);
+  return Math.round(poders.reduce((suma,poder)=>suma+poder)/poders.length)
+}
+
+// Modificar per a que mostre els superiors al que fem click
+
 (() => {
   document.addEventListener("DOMContentLoaded", () => {
+    let BulbasaurPower = calcPower(pokemons.find(p=> p.name.english === 'Bulbasaur'))
     let contenedor = document.querySelector("#pokemons");
     let total = pokemons
-      .filter((p) => p.type.includes("Grass"))
-      .map((p) => {
-        let divPokemon = document.createElement("div");
-        divPokemon.innerHTML = `<h3>${p.name.english}</h3>
+      .map(p=> { 
+        const pCopy = structuredClone(p)
+        pCopy.power = calcPower(p); 
+        return pCopy
+      })
+      .filter(p => p.power >= BulbasaurPower)
+      .map((p) =>  `<div><h3>${p.name.english}</h3>
             <p>Type: ${p.type}</p>
-            <p>Attack: ${p.base.Attack} </p>`;
-        contenedor.append(divPokemon);
-        return p;
-      });
-    let mitjana =
-      total.reduce((anterior, actual) => anterior + actual.base.Attack, 0) /
-      total.length;
-    // console.log(total, mitjana);
+            <p>Power: ${p.power} </p>
+            </div>`
+      )
+      .join('');
+    contenedor.innerHTML = total
+   
   });
 })();
 
