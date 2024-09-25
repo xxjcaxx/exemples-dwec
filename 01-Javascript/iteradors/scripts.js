0a649f34231f43351415f0fa53725627ac5da92a
xxjcaxx <jocasal@gmail.com>
2024-09-25 15:33:23 +0200

    pokemons

--- 01-Javascript/iteradors/scripts.js
+++ 01-Javascript/iteradors/scripts.js
@@ -5,27 +5,46 @@ function calcPower(pokemon){
   return Math.round(poders.reduce((suma,poder)=>suma+poder)/poders.length)
 }
 
-// Modificar per a que mostre els superiors al que fem click
-
-(() => {
-  document.addEventListener("DOMContentLoaded", () => {
-    let BulbasaurPower = calcPower(pokemons.find(p=> p.name.english === 'Bulbasaur'))
-    let contenedor = document.querySelector("#pokemons");
-    let total = pokemons
-      .map(p=> { 
-        const pCopy = structuredClone(p)
-        pCopy.power = calcPower(p); 
-        return pCopy
-      })
-      .filter(p => p.power >= BulbasaurPower)
-      .map((p) =>  `<div><h3>${p.name.english}</h3>
+function renderPokemons(pokemons){
+  let divPokemons = pokemons
+      .map((p) =>  `<div id="pokemon-${p.id}" data-type="pokemonCard">
+            <h3>${p.name.english}</h3>
             <p>Type: ${p.type}</p>
             <p>Power: ${p.power} </p>
             </div>`
       )
       .join('');
-    contenedor.innerHTML = total
-   
+      return divPokemons;
+}
+
+// Modificar per a que mostre els superiors al que fem click
+
+(() => {
+  document.addEventListener("DOMContentLoaded", () => {
+
+    let contenedor = document.querySelector("#pokemons");
+    let pokemonsPower = pokemons
+      .map(p=> { 
+        const pCopy = structuredClone(p)
+        pCopy.power = calcPower(p); 
+        return pCopy
+      })
+
+    contenedor.innerHTML = renderPokemons(pokemonsPower);
+
+    contenedor.addEventListener('click',(event)=>{
+      if(event.target.dataset.type === 'pokemonCard'){
+        let idBase = event.target.id.split('-')[1];
+        let powerBase = pokemonsPower.find(p => p.id == idBase).power;
+        contenedor.innerHTML = renderPokemons(
+          pokemonsPower.filter(p => p.power >= powerBase)
+        )
+        
+      }
+      
+      
+    });
+
   });
 })();
 
