4435be300c002f3dee04bf7c8e4d63ad9527aa17
xxjcaxx <xxjcaxx@yahoo.es>
2024-09-26 15:30:25 +0200

    bootstrap en shadow root

--- 09 - Proves/shadowdom/main.js
+++ 09 - Proves/shadowdom/main.js
@@ -1,20 +1,30 @@
 import * as bootstrap from 'bootstrap'
 import './styles.scss'
+import styles from "./styles.scss?inline"
 
 document.querySelector('#app').innerHTML = `
   <div>
   <div class="container py-4 px-3 mx-auto">
   <h1>Hello, Bootstrap and Vite!</h1>
   <button class="btn btn-primary">Primary button</button>
-</div>
-
-<div id="host">
-  <template shadowrootmode="open">
-    <span>I'm in the shadow DOM</span>
-  </template>
-</div>
-
-
-    
+</div>   
   </div>
-`
+`;
+
+const host = document.querySelector("#host");
+const shadow = host.attachShadow({ mode: "open" });
+const span = document.createElement("div");
+span.innerHTML = `
+ <style>${styles}</style>
+<button type="button" class="btn btn-primary">Primary</button>
+<button type="button" class="btn btn-secondary">Secondary</button>
+<button type="button" class="btn btn-success">Success</button>
+<button type="button" class="btn btn-danger">Danger</button>
+<button type="button" class="btn btn-warning">Warning</button>
+<button type="button" class="btn btn-info">Info</button>
+<button type="button" class="btn btn-light">Light</button>
+<button type="button" >Dark</button>
+`;
+shadow.appendChild(span);
+
+console.log(styles);
