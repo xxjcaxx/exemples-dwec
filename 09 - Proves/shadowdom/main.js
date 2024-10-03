d09a94888707cb29c5d40380f9601be7d6b94906
xxjcaxx <xxjcaxx@yahoo.es>
2024-10-03 16:38:38 +0200

    ejemplo web component reactivo

--- 09 - Proves/shadowdom/main.js
+++ 09 - Proves/shadowdom/main.js
@@ -1,34 +1,52 @@
-import * as bootstrap from 'bootstrap'
-import './styles.scss'
-import styles from "./styles.scss?inline"
+import * as bootstrap from "bootstrap";
+import "./styles.scss";
+import styles from "./styles.scss?inline";
 
+const plantillaFormulario = `
+    <div class="container mt-5">
+      <h2 class="mb-4">
+      <slot name="titulo">Formulario reactivo</slot>
+      </h2>
+      <form id="formularioReactivo">
+        <!-- Campo de texto -->
+        <div class="mb-3">
+          <label for="nombre" class="form-label">Nombre</label>
+          <input
+            type="text"
+            class="form-control"
+            id="nombre"
+            placeholder="Ingresa tu nombre"
+            required
+          />
+        </div>
 
-const plantillaFormulario = `<div class="container mt-5">
-<h2 class="mb-4">Formulario reactivo</h2>
-<form id="formularioReactivo">
-  <!-- Campo de texto -->
-  <div class="mb-3">
-    <label for="nombre" class="form-label">Nombre</label>
-    <input type="text" class="form-control" id="nombre" placeholder="Ingresa tu nombre" required>
-  </div>
-  
-  <!-- Campo de correo electrónico -->
-  <div class="mb-3">
-    <label for="email" class="form-label">Correo Electrónico</label>
-    <input type="email" class="form-control" id="email" placeholder="Ingresa tu correo electrónico" required>
-  </div>
-  
-  <!-- Botón de enviar -->
-  <button type="submit" class="btn btn-primary">Enviar</button>
-</form>
-<div>
-<p><strong>Valor Nombre:</strong> <span id="nombre-output"></span></p>
-<p><strong>Valor Correo Electrónico:</strong> <span id="email-output"></span></p>
-</div>
-</div>`;
+        <!-- Campo de correo electrónico -->
+        <div class="mb-3">
+          <label for="email" class="form-label">Correo Electrónico</label>
+          <input
+            type="email"
+            class="form-control"
+            id="email"
+            placeholder="Ingresa tu correo electrónico"
+            required
+          />
+        </div>
 
-document.addEventListener("DOMContentLoaded",()=>{
-  document.querySelector('#app').innerHTML = `
+        <!-- Botón de enviar -->
+        <button type="submit" class="btn btn-primary">Enviar</button>
+      </form>
+      <div>
+        <p><strong>Valor Nombre:</strong> <span id="nombre-output"></span></p>
+        <p>
+          <strong>Valor Correo Electrónico:</strong>
+          <span id="email-output"></span>
+        </p>
+      </div>
+    </div>
+`;
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelector("#app").innerHTML = `
   <div>
   <div class="container py-4 px-3 mx-auto">
   <h1>Hello, Bootstrap and Vite!</h1>
@@ -37,10 +55,10 @@ document.addEventListener("DOMContentLoaded",()=>{
   </div>
 `;
 
-const host = document.querySelector("#host");
-const shadow = host.attachShadow({ mode: "open" });
-const span = document.createElement("div");
-span.innerHTML = `
+  const host = document.querySelector("#host");
+  const shadow = host.attachShadow({ mode: "open" });
+  const span = document.createElement("div");
+  span.innerHTML = `
  <style>${styles}</style>
 <button type="button" class="btn btn-primary">Primary</button>
 <button type="button" class="btn btn-secondary">Secondary</button>
@@ -51,17 +69,38 @@ span.innerHTML = `
 <button type="button" class="btn btn-light">Light</button>
 <button type="button" >Dark</button>
 `;
-shadow.appendChild(span);
-console.log(styles);
+  shadow.appendChild(span);
+  console.log(styles);
 
-///////// El formulario reactivo
+  ///////// El formulario reactivo
 
-document.querySelector('#reactividad').innerHTML = plantillaFormulario;
+  class formularioReactivo extends HTMLElement {
 
-// Obtenemos el "estado" del formulario:
+    constructor() {
+      super();
 
-const formulario = document.querySelector('#formularioReactivo');
+    }
+    connectedCallback() {
+      const templateFormulario = document.createElement('template');
+      templateFormulario.innerHTML = plantillaFormulario;
+      const templateFormularioContent = templateFormulario.content;
+      const shadowRoot = this.attachShadow({ mode: "closed" });
+      shadowRoot.append(templateFormularioContent.cloneNode(true));
+    }
 
+  }
 
+  customElements.define("custom-formularioreactivo", formularioReactivo);
 
+  document.querySelector("#reactividad").innerHTML = "";
+
+  document.querySelector("#reactividad").innerHTML = `
+  <custom-formularioreactivo>
+    <span slot="titulo">Formulario reactivo con el titulo en slot</slot>
+  </custom-formularioreactivo>
+  `;
+
+  // Obtenemos el "estado" del formulario:
+
+  const formulario = document.querySelector("#formularioReactivo");
 });
