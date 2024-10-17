e50c42e07772915c43b0e6665e951a8292d0e99f
xxjcaxx <jocasal@gmail.com>
2024-10-17 17:28:21 +0200

    supabase2

--- 03 - Comunicacio_servidor/supabase/exemple2024/movies/index.js
+++ 03 - Comunicacio_servidor/supabase/exemple2024/movies/index.js
@@ -9,4 +9,66 @@ async function getAndRenderMovies(container){
 document.addEventListener('DOMContentLoaded', ()=>{
     const moviesContainer = document.querySelector('#moviesContainer');
     getAndRenderMovies(moviesContainer);
+
+    const loginButton = document.querySelector('#loginButton');
+    loginButton.addEventListener('click', async(event)=>{
+        event.preventDefault();
+        const datos = new FormData(document.querySelector('#login form'));
+        console.log(JSON.stringify(Object.fromEntries(datos)));
+        const body = JSON.stringify(Object.fromEntries(datos));
+        const response = await fetch('https://ygvtpucoxveebizknhat.supabase.co/auth/v1/token?grant_type=password',{
+            method: "POST",
+            headers: {
+                apiKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlndnRwdWNveHZlZWJpemtuaGF0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTUzNTA0ODYsImV4cCI6MjAzMDkyNjQ4Nn0.rDdWANw1LN10BunTH8TKeIAfM-EMlWpTaNyxQSbe30k",
+                "Content-type": "application/json" 
+            },
+            body
+        });
+        const dataLogin = await response.json();
+        console.log(dataLogin);
+        localStorage.setItem('token',dataLogin.access_token);
+        const responseProfile =  await fetch(`https://ygvtpucoxveebizknhat.supabase.co/rest/v1/profiles?select=*&id=eq.${dataLogin.user.id}`,{
+            method: "get",
+            headers: {
+                apiKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlndnRwdWNveHZlZWJpemtuaGF0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTUzNTA0ODYsImV4cCI6MjAzMDkyNjQ4Nn0.rDdWANw1LN10BunTH8TKeIAfM-EMlWpTaNyxQSbe30k",
+                Authorization: `Bearer ${dataLogin.access_token}`,
+                 
+            }
+        });
+        const dataProfile = await responseProfile.json();
+        console.log(dataProfile);
+        const avatarResponse = await fetch(`https://ygvtpucoxveebizknhat.supabase.co/storage/v1/object/fotos/${dataProfile[0].avatar_url}`,{
+            method: "get",
+            headers: {
+                apiKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlndnRwdWNveHZlZWJpemtuaGF0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTUzNTA0ODYsImV4cCI6MjAzMDkyNjQ4Nn0.rDdWANw1LN10BunTH8TKeIAfM-EMlWpTaNyxQSbe30k",
+                Authorization: `Bearer ${dataLogin.access_token}`,
+                 
+            }
+        });
+        const avatarImage = await avatarResponse.blob();
+        console.log(avatarImage);
+        
+        const avatarImageURL = URL.createObjectURL(avatarImage);
+
+        document.querySelector('#avatar').src=avatarImageURL
+        
+    });
+
+    const registerButton = document.querySelector('#registerButton');
+    registerButton.addEventListener('click', async(event)=>{
+        event.preventDefault();
+        const datos = new FormData(document.querySelector('#login form'));
+        console.log(JSON.stringify(Object.fromEntries(datos)));
+        const body = JSON.stringify(Object.fromEntries(datos));
+        const response = await fetch('https://ygvtpucoxveebizknhat.supabase.co/auth/v1/signup',{
+            method: "POST",
+            headers: {
+                apiKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlndnRwdWNveHZlZWJpemtuaGF0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTUzNTA0ODYsImV4cCI6MjAzMDkyNjQ4Nn0.rDdWANw1LN10BunTH8TKeIAfM-EMlWpTaNyxQSbe30k",
+                "Content-type": "application/json" 
+            },
+            body
+        });
+        const dataLogin = await response.json();
+        console.log(dataLogin);        
+    });
 });
--- 03 - Comunicacio_servidor/supabase/exemple2024/movies/services/supaservice.js
+++ 03 - Comunicacio_servidor/supabase/exemple2024/movies/services/supaservice.js
@@ -1,12 +1,13 @@
 export {getMovies}
 
 async function getMovies(){
+    const token = localStorage.getItem('token');
    const response = await fetch("https://ygvtpucoxveebizknhat.supabase.co/rest/v1/movies?select=*",{
         method: "get",
         headers: {
             apiKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlndnRwdWNveHZlZWJpemtuaGF0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTUzNTA0ODYsImV4cCI6MjAzMDkyNjQ4Nn0.rDdWANw1LN10BunTH8TKeIAfM-EMlWpTaNyxQSbe30k",
-            Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlndnRwdWNveHZlZWJpemtuaGF0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTUzNTA0ODYsImV4cCI6MjAzMDkyNjQ4Nn0.rDdWANw1LN10BunTH8TKeIAfM-EMlWpTaNyxQSbe30k",
-            
+            Authorization: `Bearer ${token}`,
+             
         }
     });
     const movies = await response.json();
