08a7515ec489b97fae837d4560c85112e222d3f8
xxjcaxx <xxjcaxx@yahoo.es>
2024-12-19 14:50:44 +0100

    recipes

---
+++ 08 - angular/meals/src/app/components/footer/footer.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  imports: [],
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.css',
+  standalone: true
+})
+export class FooterComponent {
+
+}
---
+++ 08 - angular/meals/src/app/components/header/header.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  imports: [],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css',
+  standalone: true
+})
+export class HeaderComponent {
+
+}
