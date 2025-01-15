ebbdd732ebf2f6e9805d4a5d463ba96ce6aeeac1
xxjcaxx <jocasal@gmail.com>
2025-01-15 14:59:44 +0100

    vgx

---
+++ 08 - angular/2425/myrecipes/.angular/cache/19.0.5/myrecipes/vite/deps/browser-LV3Q5H36.js
@@ -0,0 +1,14 @@
+import {
+  __commonJS
+} from "./chunk-S35DAJRX.js";
+
+// node_modules/@supabase/realtime-js/node_modules/ws/browser.js
+var require_browser = __commonJS({
+  "node_modules/@supabase/realtime-js/node_modules/ws/browser.js"(exports, module) {
+    module.exports = function() {
+      throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object");
+    };
+  }
+});
+export default require_browser();
+//# sourceMappingURL=browser-LV3Q5H36.js.map
---
+++ 08 - angular/2425/myrecipes/.angular/cache/19.0.5/myrecipes/vite/deps/browser-U366SZMB.js
@@ -0,0 +1,18 @@
+import {
+  Headers,
+  Request,
+  Response,
+  browser_default,
+  fetch,
+  init_browser
+} from "./chunk-5JB4KNXG.js";
+import "./chunk-S35DAJRX.js";
+init_browser();
+export {
+  Headers,
+  Request,
+  Response,
+  browser_default as default,
+  fetch
+};
+//# sourceMappingURL=browser-U366SZMB.js.map
