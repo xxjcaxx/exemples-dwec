79a6c056e4c4f1fbc0cedd14e7610804bca55332
xxjcaxx <xxjcaxx@yahoo.es>
2025-06-05 09:55:18 +0200

    custom forms

--- 08-WebComponents/exempleformcontrol/index.js
+++ 08-WebComponents/exempleformcontrol/index.js
@@ -8,6 +8,8 @@ customElements.define('my-input', class extends HTMLElement {
     static formAssociated = true;
     #internals;
     #shouldFireChange = false;
+    #formDisabled = false;
+    #value;
 
     constructor() {
         super();
@@ -19,11 +21,43 @@ customElements.define('my-input', class extends HTMLElement {
         this.addEventListener('focusout', this);
     }
     
+    set value(v) {
+        if (this.#value !== String(v)) {
+            this.#value = String(v);
+            this.#update();    
+        }
+    }
     get value() {
+        return this.#value ?? this.defaultValue;
+    }
+
+    get defaultValue() {
         return this.getAttribute('value') ?? '';
     }
-    set value(value) {
+    set defaultValue(value) {
         this.setAttribute('value', String(value));
+    }
+
+    set disabled(v) {
+        if (v) {
+            this.setAttribute('disabled', 'true');
+        } else {
+            this.removeAttribute('disabled');
+        }
+    }
+    get disabled() {
+        return this.hasAttribute('disabled');
+    }
+
+    set readOnly(v) {
+        if (v) {
+            this.setAttribute('readonly', 'true');
+        } else {
+            this.removeAttribute('readonly');
+        }
+    }
+    get readOnly() {
+        return this.hasAttribute('readonly');
     }
 
     get name() {
@@ -39,16 +73,22 @@ customElements.define('my-input', class extends HTMLElement {
         this.#update();
     }
 
-    static observedAttributes = ['value'];
+    static observedAttributes = ['value', 'disabled', 'readonly'];
     attributeChangedCallback() {
         this.#update();
     }
 
+
     #update() {
-        if (this.textContent !== this.value) {
-            this.textContent = this.value;
-        }
+        this.style.display = 'inline';
+        this.textContent = this.value;
         this.#internals.setFormValue(this.value);
+
+        const isDisabled = this.#formDisabled || this.disabled;
+        this.#internals.ariaDisabled = isDisabled;
+        this.#internals.ariaReadOnly = this.readOnly;
+        this.contentEditable = !this.readOnly && !isDisabled && 'plaintext-only';
+        this.tabIndex = isDisabled ? -1 : 0;
     }
 
     handleEvent(e) {
@@ -90,6 +130,22 @@ customElements.define('my-input', class extends HTMLElement {
                 }
                 break;
         }
+    }
+
+
+    formResetCallback() {
+        this.#value = undefined;
+        this.#update();
+    }
+    
+    formDisabledCallback(disabled) {
+        this.#formDisabled = disabled;
+        this.#update();
+    }
+    
+    formStateRestoreCallback(state) {
+        this.#value = state ?? undefined;
+        this.#update();
     }
 });
 
