b6cd5de1048049520f0204bddbacf9cbcf47249b
xxjcaxx <jocasal@gmail.com>
2025-09-25 09:33:36 +0200

    buscar

--- 01-Javascript/pokemons25/index.js
+++ 01-Javascript/pokemons25/index.js
@@ -1,5 +1,5 @@
 function renderPokemon(p) {
-  const pokemonDivContent = `<div class="pokemonCard">
+  const pokemonDivContent = `<div class="pokemonCard" data-id-pokemon="${p.id}">
     <h2>${p.name.english}</h2>
     <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${p.id}.png">
     <div >
@@ -11,11 +11,12 @@ function renderPokemon(p) {
   return pokemonDiv.firstChild;
 }
 
-function printPokemons(pokemons){
-  const pokemonDivs = pokemons.map(renderPokemon)
+function printPokemons(pokemons) {
+  const pokemonDivs = pokemons.map(renderPokemon);
   const container = document.querySelector("#container");
-  container.innerHTML = "";
-  container.append(...pokemonDivs);
+  /*container.innerHTML = "";
+  container.append(...pokemonDivs);*/
+  container.replaceChildren(...pokemonDivs);
 }
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -33,24 +34,51 @@ document.addEventListener("DOMContentLoaded", () => {
     buttonTipo.innerText = t;
     buttonTipo.addEventListener("click", () => {
       console.log(t);
-      const filteredPokemons = pokemons.filter(p => p.type.includes(t));
-      printPokemons(filteredPokemons.slice(0,20));
+      const filteredPokemons = pokemons.filter((p) => p.type.includes(t));
+      printPokemons(filteredPokemons.slice(0, 20));
     });
     return buttonTipo;
   });
 
   const navBar = document.querySelector("#navbar");
-  const resetButton = document.createElement('button');
-  resetButton.innerText = "Reset";
-  resetButton.addEventListener("click",()=>{
-    printPokemons(pokemons.slice(0, 10));
 
-  })
-  navBar.append(resetButton,...botons);
+  const resetButton = document.createElement("button");
+  resetButton.innerText = "Reset";
+  resetButton.addEventListener("click", () => {
+    printPokemons(pokemons.slice(0, 10));
+  });
+
+  const sortButtons = `
+  <button id="sort-name">Sort By name</button>
+  <form id="buscador">
+    <input type="text" id="input-search" placeholder="buscar...">
+    <button type="submit" id="button-search">Buscar</button>
+</form>
+  `;
+  const divSortButtons = document.createElement("div");
+  divSortButtons.innerHTML = sortButtons;
+  const sortButton = divSortButtons.querySelector("#sort-name");
+  sortButton.addEventListener("click", () => {
+    const sortedPokemons = [...pokemons].sort((a, b) => {     
+     return a.name.english < b.name.english ? -1 : 1
+    }
+    );
+    printPokemons(sortedPokemons.slice(0, 20));
+  });
+
+  const buttonSearch = divSortButtons.querySelector('#button-search');
+  buttonSearch.addEventListener('click',(event)=>{
+    event.preventDefault();
+    const searchText = divSortButtons.querySelector('#input-search').value;
+    const filteredPokemons = pokemons.filter(p=> p.name.english.toLowerCase().includes(searchText.toLowerCase()))
+     printPokemons(filteredPokemons.slice(0, 20));
+  });
+
+  navBar.append(divSortButtons, resetButton, ...botons);
 
   // Fer un buscador per nom
 
-  // Fer botons per ordenar 
+  // Fer botons per ordenar
 
   // Fer paginació
 });
