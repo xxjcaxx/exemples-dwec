cba5079f7a70dd14245f837b8942778a3726c26f
xxjcaxx <jocasal@gmail.com>
2025-10-16 10:37:21 +0200

    alertes

--- 03 - Comunicacio_servidor/supabase/01_primer_exemple/supabase.js
+++ 03 - Comunicacio_servidor/supabase/01_primer_exemple/supabase.js
@@ -1,10 +1,10 @@
 async function fetchProducts(){
-   let response = await fetch('https://sxmhalwwwortzueczlno.supabase.co/rest/v1/products?select=*',
+   let response = await fetch('https://zjwnfbhnemehixhiupey.supabase.co/rest/v1/products?select=*',
         {
             method: 'get',
             headers: {
-                    "apikey": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN4bWhhbHd3d29ydHp1ZWN6bG5vIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTkyNjYzMDIsImV4cCI6MjAxNDg0MjMwMn0.6t5wygarFGkpH3N3UjWYmEnoEdDeB2zsfgZevCl9VPo",
-                    "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN4bWhhbHd3d29ydHp1ZWN6bG5vIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTkyNjYzMDIsImV4cCI6MjAxNDg0MjMwMn0.6t5wygarFGkpH3N3UjWYmEnoEdDeB2zsfgZevCl9VPo"
+                    "apikey": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inpqd25mYmhuZW1laGl4aGl1cGV5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA1MzIxMzgsImV4cCI6MjA3NjEwODEzOH0.3cNnBxWPVvpfr0vcFvbr2fxz2y20ZW2GNS6IZS6pHK0",
+                    "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inpqd25mYmhuZW1laGl4aGl1cGV5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA1MzIxMzgsImV4cCI6MjA3NjEwODEzOH0.3cNnBxWPVvpfr0vcFvbr2fxz2y20ZW2GNS6IZS6pHK0"
             }
             });
     let data = await response.json();
--- 03 - Comunicacio_servidor/transitmassachussets/rick/index.js
+++ 03 - Comunicacio_servidor/transitmassachussets/rick/index.js
@@ -1,16 +1,71 @@
+let colorArray = [
+  "#FF6633",
+  "#E6FF80",
+  "#FF33FF",
+  "#FFFF99",
+  "#00B3E6",
+  "#E6B333",
+  "#3366E6",
+  "#999966",
+  "#99FF99",
+  "#B34D4D",
+  "#80B300",
+  "#809900",
+  "#E6B3B3",
+  "#6680B3",
+  "#66991A",
+  "#FF99E6",
+  "#CCFF1A",
+  "#FF1A66",
+  "#E6331A",
+  "#33FFCC",
+  "#66994D",
+  "#B366CC",
+  "#4D8000",
+  "#B33300",
+  "#CC80CC",
+  "#66664D",
+  "#991AFF",
+  "#E666FF",
+  "#4DB3FF",
+  "#1AB399",
+  "#E666B3",
+  "#33991A",
+  "#CC9999",
+  "#B3B31A",
+  "#00E680",
+  "#4D8066",
+  "#809980",
+  "#E6FF80",
+  "#1AFF33",
+  "#999933",
+  "#FF3380",
+  "#CCCC00",
+  "#66E64D",
+  "#4D80CC",
+  "#9900B3",
+  "#E64D66",
+  "#4DB380",
+  "#FF4D4D",
+  "#99E6E6",
+  "#6666FF",
+];
+/*
 const getAlerts = (url) =>
-    fetch(url)
-    .then(response => response.json())
-    .then(data => data.data.map(a => a.attributes))
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => data.data.map((a) => a.attributes));
+*/
 
+const getAlerts = async (url) => {
+    const response = await fetch(url);
+    const data = await response.json();
+    return  data.data.map((a) => a.attributes);
+}
 
-
-
-document.addEventListener("DOMContentLoaded",()=>{
-    const container = document.querySelector('#container');
-    const tableAlerts = document.createElement('table');
-    container.append(tableAlerts);
-    tableAlerts.innerHTML= `
+const renderAlerts = (alerts) => {
+  const tableAlerts = document.createElement("table");
+  tableAlerts.innerHTML = `
                 <thead>
                 <th>
                     Cause
@@ -22,18 +77,63 @@ document.addEventListener("DOMContentLoaded",()=>{
                     Effect
                 </th>
             </thead>
-    `
+    `;
 
-   getAlerts("https://api-v3.mbta.com/alerts?sort=-created_at&filter%5Bactivity%5D=BOARD%2CEXIT%2CRIDE").then(alerts =>
-   {
-    
-     const trs = alerts.map(a=> {
-        const tr = document.createElement('tr');
-        tr.innerHTML = `<td>${a.cause}</td><td>${a.header}</td><td>${a.effect}</td>`;
-        return tr
-     });
-     tableAlerts.append(...trs)
-   }
-   )
+  const causes = [...new Set(alerts.map((a) => a.cause))];
 
+  /*const trs = alerts.map((a) => {
+    const tr = document.createElement("tr");
+    tr.style.backgroundColor = colorArray[causes.indexOf(a.cause)];
+    tr.innerHTML = `<td>${a.cause}</td><td>${a.header}</td><td>${a.effect}</td>`;
+    return tr;
+  });
+  tableAlerts.append(...trs);*/
+
+  for(let a of alerts){
+     const tr = document.createElement("tr");
+    tr.style.backgroundColor = colorArray[causes.indexOf(a.cause)];
+    tr.innerHTML = `<td>${a.cause}</td><td>${a.header}</td><td>${a.effect}</td>`;
+    tableAlerts.append(tr);
+  }
+
+  return tableAlerts;
+};
+/*
+const manageAlerts = (url, time, callback, filterFunction) => {
+  const intervalIdentificator = setInterval(() => {
+    getAlerts(url)
+    .then(alerts => alerts.filter(filterFunction))
+    .then(callback);
+  }, time);
+  return intervalIdentificator;
+};
+*/
+
+const manageAlerts = (url, time, callback, filterFunction) => {
+  const intervalIdentificator = setInterval(async () => {
+    let alerts = await getAlerts(url);
+    alerts = alerts.filter(filterFunction);
+    callback(alerts);
+  }, time);
+  return intervalIdentificator;
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  const container = document.querySelector("#container");
+  const url =
+    "https://api-v3.mbta.com/alerts?sort=-created_at&filter%5Bactivity%5D=BOARD%2CEXIT%2CRIDE";
+  const intervalIdentificator=  manageAlerts(url, 3000, (alerts) => {
+    const tableAlerts = renderAlerts(alerts);
+    container.replaceChildren(tableAlerts);
+  }, (alert)=> alert.cause === "CONSTRUCTION");
+
+   
+ 
+  /*
+  getAlerts(
+    "https://api-v3.mbta.com/alerts?sort=-created_at&filter%5Bactivity%5D=BOARD%2CEXIT%2CRIDE"
+  ).then((alerts) => {
+    const tableAlerts = renderAlerts(alerts);
+    container.append(tableAlerts);
+  });*/
 });
