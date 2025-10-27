26be71e90067c467104859ca180db9c57459d5cc
xxjcaxx <jocasal@gmail.com>
2025-10-27 13:20:33 +0100

    repas bicis

--- 03 - Comunicacio_servidor/supabase/exemplebasic/supa.js
+++ 03 - Comunicacio_servidor/supabase/exemplebasic/supa.js
@@ -1,10 +1,15 @@
+import { SUPABASE_KEY } from "./environment.js";
+
 (() => {
     document.addEventListener('DOMContentLoaded', () => {
 
-        const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJxaG52d2Zvdm1jeHJxcnNtZnhyIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjYyNzk4MDYsImV4cCI6MTk4MTg1NTgwNn0.jVhmEO__GFSxqRlbzdCxyeb_VxWWD7Bqk9sj3Po8xtM"
+      //  const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inpqd25mYmhuZW1laGl4aGl1cGV5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA1MzIxMzgsImV4cCI6MjA3NjEwODEzOH0.3cNnBxWPVvpfr0vcFvbr2fxz2y20ZW2GNS6IZS6pHK0"
         let access_token = '';
+        if(localStorage.getItem('access_token')){
+            access_token = localStorage.getItem('access_token')
+        }
      /*   let token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJhdXRoZW50aWNhdGVkIiwiZXhwIjoxNjY3MDU0ODc0LCJzdWIiOiJiMDc5ODMyZC1lMjQzLTRhYWYtYmQ5Mi1iNGZiOTYxZTg2NzQiLCJlbWFpbCI6ImpvY2FzYWxAZ21haWwuY29tIiwicGhvbmUiOiIiLCJhcHBfbWV0YWRhdGEiOnsicHJvdmlkZXIiOiJlbWFpbCIsInByb3ZpZGVycyI6WyJlbWFpbCJdfSwidXNlcl9tZXRhZGF0YSI6e30sInJvbGUiOiJhdXRoZW50aWNhdGVkIiwic2Vzc2lvbl9pZCI6IjQyOTQzZDI2LWEyNWUtNGEzMi1iODgzLWNhNzFkNTQyZTUxYSJ9.lFTwGO-2dbhPMg7XBMg2EXU4iBjmeQrvWMbbw798GPE"
-        fetch('https://bqhnvwfovmcxrqrsmfxr.supabase.co/rest/v1/graphs',{ 
+        fetch('https://zjwnfbhnemehixhiupey.supabase.co/rest/v1/graphs',{ 
             method: 'get',
             headers: {
                 "apiKey": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJxaG52d2Zvdm1jeHJxcnNtZnhyIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjYyNzk4MDYsImV4cCI6MTk4MTg1NTgwNn0.jVhmEO__GFSxqRlbzdCxyeb_VxWWD7Bqk9sj3Po8xtM",
@@ -22,7 +27,7 @@
                     email:  document.querySelector('#signupemail').value,
                     password:  document.querySelector('#signuppassword').value 
                 };
-                let response = await fetch('https://bqhnvwfovmcxrqrsmfxr.supabase.co/auth/v1/signup',{
+                let response = await fetch('https://zjwnfbhnemehixhiupey.supabase.co/auth/v1/signup',{
                     method: 'post',
                     headers: {
                         "apiKey": SUPABASE_KEY,
@@ -39,7 +44,7 @@
                     email:  document.querySelector('#loginemail').value,
                     password:  document.querySelector('#loginpassword').value 
                 };
-                let response = await fetch('https://bqhnvwfovmcxrqrsmfxr.supabase.co/auth/v1/token?grant_type=password',{
+                let response = await fetch('https://zjwnfbhnemehixhiupey.supabase.co/auth/v1/token?grant_type=password',{
                     method: 'post',
                     headers: {
                         "apiKey": SUPABASE_KEY,
@@ -50,13 +55,14 @@
                 let data = await response.json();
                 document.querySelector('#userInfo').innerHTML = JSON.stringify(data);
                 access_token = data.access_token;
+                localStorage.setItem('access_token',access_token);
 
             });
 
 
 
             document.querySelector('#pedirDatos').addEventListener('click',()=>{
-                fetch('https://bqhnvwfovmcxrqrsmfxr.supabase.co/rest/v1/graphs',{ 
+                fetch('https://zjwnfbhnemehixhiupey.supabase.co/rest/v1/products?select=*',{ 
                     method: 'get',
                     headers: {
                         "apiKey": SUPABASE_KEY,
---
+++ examens_javascript/repasos/bicis/src/main.js
@@ -0,0 +1,156 @@
+// Import our custom CSS
+import "./style.scss";
+
+// Import all of Bootstrap’s JS
+import * as bootstrap from "bootstrap";
+
+const getBikes = async (limit, offset) => {
+  const response = await fetch(
+    `https://public.opendatasoft.com/api/explore/v2.1/catalog/datasets/jcdecaux-bike-stations-data-rt/records/?limit=${limit}&offset=${offset}`
+  );
+  const data = await response.json();
+  return data;
+};
+
+const renderBikes = (bikes) => {
+  const divBikes = document.createElement("div");
+  divBikes.innerHTML = `
+<div class="container">
+  <div class="row">
+      ${bikes
+        .map(
+          (b) => `
+          <div class="col">
+          <div class="card" style="width: 18rem;">
+            <div class="card-body">
+              <h5 class="card-title">${b.name}</h5>
+              <h6 class="card-subtitle mb-2 text-body-secondary">${b.address}</h6>
+              <p class="card-text">available_bikes: ${b.available_bikes} <br>
+              bike_stands ${b.bike_stands} <br>
+              </p>
+
+            </div>
+          </div>
+          </div>
+    `
+        )
+        .join("")}
+  </div>
+</div>
+  `;
+
+  return divBikes;
+};
+
+const renderPages = (pages) => {
+  const divPages = document.createElement("div");
+  divPages.innerHTML = `
+    <div class="btn-group me-2" role="group" aria-label="First group" id="buttonsGroup">
+        ${
+          /*pages
+          .map(
+            (p) => `
+            <button type="button" class="btn btn-primary" data-page="${p}">${
+              p === -1 ? "..." : p
+            }</button>
+            `
+          )
+          .join("")*/ ""
+        }
+    </div>
+  `;
+  const buttonsGroup = divPages.querySelector("#buttonsGroup");
+
+  const renderButton = (p) => {
+    const button = document.createElement("button");
+    button.classList.add("btn", "btn-primary");
+    button.dataset.page = p;
+    button.textContent = p === -1 ? "..." : p;
+    //buttonsGroup.append(button);
+    return button;
+  };
+
+  buttonsGroup.append(...pages.map(renderButton));
+
+  divPages.addEventListener("click", (event) => {
+    if (event.target.dataset.page) {
+      const page = event.target.dataset.page;
+      const customEvent = new CustomEvent("pageChange", {
+        bubbles: true, // para que se propague
+        detail: { page },
+      });
+      divPages.dispatchEvent(customEvent);
+    }
+  });
+  return divPages;
+};
+
+const getPageAndRender = () => {
+  
+  let pageCache = {};
+
+  return async function (page,container, map) {
+    const offset = page * 10;
+    const limit = 10;
+    let dataBikes = {};
+    if(page in pageCache){
+      dataBikes = pageCache[page];
+    }
+    else {
+      dataBikes = await getBikes(limit, offset);
+      pageCache[page] = dataBikes;
+    }
+    
+    const totalPages = Math.ceil(dataBikes.total_count / 10);
+    let currentPage = page;
+    const visiblePages = Array(totalPages)
+      .fill(0)
+      .map((v, i) => i)
+      .filter(
+        (v) =>
+          v < 3 ||
+          (v > currentPage - 3 && v < currentPage + 3) ||
+          v > totalPages - 4
+      )
+      .map((v, i, pages) => (Math.abs(pages[i + 1] - v) > 1 ? [v, -1] : v))
+      .flat();
+    console.log(totalPages, currentPage, visiblePages);
+
+    const divBikes = renderBikes(dataBikes.results);
+    const divPages = renderPages(visiblePages, currentPage);
+
+    dataBikes.results.forEach((b) => {
+      L.marker([b.position.lat, b.position.lon]).addTo(map).bindPopup(b.name);
+    });
+
+    const available_bikes = dataBikes.results.reduce(
+      (suma, b) => {
+        suma.available_bikes += b.available_bikes;
+        return suma;
+      },
+      { available_bikes: 0 }
+    );
+    console.log(available_bikes);
+
+    container.replaceChildren(divBikes, divPages);
+  };
+};
+
+document.addEventListener("DOMContentLoaded", async () => {
+  const container = document.querySelector("#app");
+  const getPageCache = getPageAndRender();
+
+  let map = L.map("map").setView([51.505, -0.09], 5);
+
+  L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  }).addTo(map);
+
+  getPageCache(17,container, map);
+
+  container.addEventListener("pageChange", (event) => {
+    console.log(event.detail);
+    getPageCache(event.detail.page,container,map);
+  });
+});
